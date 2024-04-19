4d16a87dcfe472a6c87781dfe608aef177cb44e6
root <bousfihahassan2019@gmail.com>
2024-04-19 07:29:38 -0700

    a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+class install_flask {
+    # Install Flask using pip3
+    package { 'flask':
+        ensure   => '2.1.0',
+        provider => 'pip3',
+    }
 }
