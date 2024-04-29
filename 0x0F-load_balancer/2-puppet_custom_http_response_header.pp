2de99deedbf43e26ba89bf219b3fbac1f4c9d0e5
BousfihaHassan <bousfihahassan2019@gmail.com>
2024-04-29 14:52:37 -0700

    send

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,4 +1,4 @@
-e Puppet to automate the task of creating a custom HTTP header response
+#Use Puppet to automate the task of creating a custom HTTP header response
 
 exec {'update':
   command => '/usr/bin/apt-get update',
